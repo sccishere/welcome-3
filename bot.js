9fee565bc0f3a742048e7260ad9051bce0529532
sccishere <40580750+sccishere@users.noreply.github.com>
2018-06-26 17:39:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,117 +28,42 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
 });
 
-
-
-let sw = JSON.parse(fs.readFileSync("./setWlc.json", "UTF8"))
- 
-    client.on('message', message => {
-const Canvas = require("canvas") // npm i canvas
-const fs = require("fs") // npm i fs
- 
-        let mothed = ['text', 'embed', 'image'];
-        let sets = message.content.split(" ").slice(1).join(" ")
-        let style = message.content.split(" ").slice(2).join(" ")
-        let stym = message.content.split(" ").slice(3).join(" ")
-        let msz = message.content.split(" ").slice(2).join(" ")
-        let ch = message.content.split(" ").slice(2).join(" ")
-        let r = message.content.split(" ").slice(4).join(" ")
- 
- 
-        if(message.content.startsWith(prefix + "setWlc")) {
-    if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**You need `Manage Channels` permission**")
-            if(!sw[message.guild.id]) sw[message.guild.id] = {
-                cha: "welcome",
-                msz: "Welcome Bro",
-                styler: "text"
-            };
- 
-            if(!sets) {
-                message.channel.send(`**Usage:
-            ${prefix}setWlc style <text, image, embed>
-            ${prefix}setWlc msg <message>
-            ${prefix}setWlc channal <channel name>**`)
-            }
- 
-            if(!mothed) {
-                message.channel.send(`**Usage: ${prefix}setWlc style <text, imgae, embed>**`)
-            }
- 
-            if(message.content === prefix + 'setWlc style image') {
-                if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**You need `Manage Channels` permission**")
-                sw[message.guild.id].styler = 'image'
-                message.channel.send(`**Your server welcome mothed has been changed to ${sw[message.guild.id].styler}**`)
-            }
- 
-            if(message.content === prefix + 'setWlc style embed') {
-                if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**You need `Manage Channels` permission**")
-                 sw[message.guild.id].styler = 'embed'
-                message.channel.send(`**Your server welcome mothed has been changed to ${sw[message.guild.id].styler}**`)            }
- 
-            if(message.content === prefix + 'setWlc style text') {
-                if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**You need `Manage Channels` permission**")
-                 sw[message.guild.id].styler = 'text'
-                message.channel.send(`**Your server welcome mothed has been changed to ${sw[message.guild.id].styler}**`)
-            }
- 
+client.on('message', message=>{
+    if (message.content === "!welcon"){
+        if (message.channel.guild) { // if message from the guild , not private 
+            if (message.member.hasPermission(8)){ // 8 = Administrator
+            fs.writeFile('./welc.json', JSON.stringify(message.guild.id, 'on')) // writing on JSON 
+            message.channel.sendMessage('Done') //ON!
+        } else {
+        message.channel.sendMessage('You CANT EVER DO THAT COMMAND !')
+    }
+    }else {
+            message.channel.sendMessage('you cant use this command here,') 
         }
- 
-        if(message.content.startsWith(prefix + "setWlc msg")) {
-            if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("**You need `Manage Messages` permission**")
-            if(!msz) {
-                message.channel.send("Usage: <setWlc msg <message>")
-            } else {
-                message.channel.send(`**Your server welcome message has been changed to __${msz}__**`)
-                sw[message.guild.id].msk = msz
+    }
+});
+client.on('message',message =>{
+    if (message.content==="!welcoff"){ // turn off the img welcome :palm_face:
+        if (message.channel.guild) { // if message from the guild , not private 
+            if (message.member.hasPermission(8)){ // if member have Administrator permission  , ( 8 = Adminstrator)
+                fs.writeFile('welc.json', JSON.stringify(message.guild.id, 'off')) // writing on FS ( JSON ) as off
+                message.channel.sendMessage('Done') // OFF!
             }
+        } else{
+            message.channel.sendMessage('YOU CANT USE THIS COMMAND HERE! :x:') //if someone send this command on private will reply
         }
- 
-        if(message.content.startsWith(prefix + "setWlc channel")) {
-            if(!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("**You need `Manage Channels` permission**")
-            if(!ch) {
-                message.channel.send("Usage: <setWlc channel <channel name>")
-            }
-            let chn = message.guild.channels.find("name", ch)
-            if(!chn) {
-                message.channel.send("**I can't find this channel**")
-            }
-            else {
-                 sw[message.guild.id].cha = chn.name
-                 message.channel.send(`**Your server welcome channel has been changed to __${chn.name}__**`)
-                 }
-        }
- 
-        fs.writeFile('./setWlc.json', JSON.stringify(sw), (err) => {
-if (err) console.error(err);
+    }
 })
-})
- 
- 
 client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find("name", sw[member.guild.id].cha)
- 
-    if(sw[member.guild.id].styler === "text") {
-        channel.sendMessage(`<@${member.user.id}>, ${sw[member.guild.id].msk}`)
-    }
- 
-    if(sw[member.guild.id].styler === "embed") {
- 
-        const embed = new Discord.RichEmbed()
-        .setTitle("Member joind.")
-        .setColor("GREEN")
-        .setThumbnail(member.user.avatarURL)
-        .setDescription(`**${sw[member.guild.id].msk}**`)
-        .addField("**Member name**", `[<@${member.user.id}>]`,true)
-        .addField("**Now we are**", `[${member.guild.memberCount}]`,true)
-        channel.sendMessage(`<@${member.user.id}>`)
-        channel.sendEmbed(embed)
-    }
- 
-    if(sw[member.guild.id].styler === "image") {
-        if (member.user.bot) return;
-const w = ['./image.png'];
+var Canvas = require('canvas') //npm i canvas
+var jimp = require('jimp') //npm i jimp
+
+const w = ['./img/w1.jpg','./img/w2.jpg','./img/w3.jpg','./img/w4.jpg','./img/w6.jpg','./img/w7.png','./img/w8.png',
+'./img/w9.png','./img/w10.png','./img/w11.png','./img/w12.png','./img/w13.png','./img/w14.png','./img/w15.png','./img/w16.png'
+,'./img/kk.png','./img/rb.png']; // you need to add Image welcome [ const w = [ './img/w1.jpg','./img/w2.jpg']; ] 
+
         let Image = Canvas.Image,
-            canvas = new Canvas(749, 198),
+            canvas = new Canvas(401, 202),
             ctx = canvas.getContext('2d');
         ctx.patternQuality = 'bilinear';
         ctx.filter = 'bilinear';
@@ -146,64 +71,50 @@ const w = ['./image.png'];
         ctx.shadowColor = 'rgba(0, 0, 0, 0.4)';
         ctx.shadowOffsetY = 2;
         ctx.shadowBlur = 2;
-        ctx.stroke();
-        ctx.beginPath();
- 
         fs.readFile(`${w[Math.floor(Math.random() * w.length)]}`, function (err, Background) {
             if (err) return console.log(err);
             let BG = Canvas.Image;
             let ground = new Image;
             ground.src = Background;
-            ctx.drawImage(ground, 0, 0, 749, 198);
- 
+            ctx.drawImage(ground, 0, 0, 401, 202);
+
 })
- 
+
                 let url = member.user.displayAvatarURL.endsWith(".webp") ? member.user.displayAvatarURL.slice(5, -20) + ".png" : member.user.displayAvatarURL;
                 jimp.read(url, (err, ava) => {
                     if (err) return console.log(err);
                     ava.getBuffer(jimp.MIME_PNG, (err, buf) => {
-                 if (err) return console.log(err);
- 
-ctx.font = '35px Aeland';
-                        ctx.fontSize = '40px';
-                        ctx.fillStyle = "#FFFFFF";
-                        ctx.textAlign = "center";
-                        ctx.fillText(" Welcome to " + member.guild.name , 440, 25);
- 
-                        //ur name
-                        ctx.font = '40px Impact';
-                        ctx.fontSize = '48px';
-                        ctx.fillStyle = "#FFFFFF";
-                        ctx.textAlign = "center";
-                        ctx.fillText(member.user.username, 420, 100);
- 
-                         ctx.font = '30px Impact';
-                        ctx.fontSize = '20px';
-                        ctx.fillStyle = "#FFFFFF";
-                        ctx.textAlign = "center";
-                        ctx.fillText(sw[member.guild.id].msk, 410, 170);
- 
- 
+                        if (err) return console.log(err);
+
                         //Avatar
                         let Avatar = Canvas.Image;
-                              let ava = new Avatar;
-                              ava.src = buf;
-                              ctx.beginPath();
-                              ctx.arc(115, 100, 90, 0, Math.PI*2);
-                                 ctx.closePath();
-                                 ctx.clip();
-                                 ctx.drawImage(ava, 5, 5, 200, 200);
-                                 channel.sendMessage(`<@${member.user.id}>`)
-        channel.sendFile(canvas.toBuffer())
- 
- 
- 
+                        let ava = new Avatar;
+                        ava.src = buf;
+                        ctx.drawImage(ava, 152, 27, 95, 95);
+
+                                                //wl
+                        ctx.font = '20px Arial Bold';
+                        ctx.fontSize = '25px';
+                        ctx.fillStyle = "#FFFFFF";
+                        ctx.textAlign = "center";
+                        ctx.fillText(`You are the  ${member.guild.memberCount}th`, 200, 190); //MemberCount
+
+                        //ur name
+                        ctx.font = '20px Arial';
+                        ctx.fontSize = '28px';
+                        ctx.fillStyle = "#FFFFFF";
+                        ctx.textAlign = "center";
+                        ctx.fillText(member.user.username, 200, 154); //shows username!
+
+member.guild.channels.find('id', welc[member.guild.id].imgwelc).sendFile(canvas.toBuffer());
+
+
+
+
 })
 })
- 
-    }
- 
-})
+
+});
 
 
 
